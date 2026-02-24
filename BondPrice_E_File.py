dba77088ad6be35f3b7e4d2ef70e7a6e875e469c
lily158-lg <lg3461@columbia.edu>
2026-02-23 22:56:57 -0500

    HW_CLASS 5

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,9 @@
-
-
-def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+def getBondPrice_E(face, couponRate, m, yc):
+    pvcfsum=0
+    cf=couponRate*face
+    for t, y in enumerate(yc, start=1):
+        pv =(1+y)**-t
+        pvcf = pv*cf
+        pvcfsum+=pvcf 
+    bondprice = pvcfsum +pv*face
+    return(bondprice)
--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,12 @@
 
 
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    #change yield and time
+    pvcfsum=0
+    cf=couponRate*face
+    for timeZ, ycZ in zip(times, yc): # times and yc are in next cell. link each time with corropoding yield #changed 
+        pv =(1+ycZ)**-timeZ  # changed 
+        pvcf = pv*cf
+        pvcfsum+=pvcf 
+    bondprice = pvcfsum +pv*face
+    return(bondprice)
